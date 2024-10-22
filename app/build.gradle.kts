12c435991404df901c4ba61989f7ab5df67209bc
hayoung <jbg01200249@gmail.com>
2024-10-22 20:00:45 +0900

    [feat] firebase 인증 서비스

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+
+    //id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -39,7 +42,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -48,4 +50,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(platform("com.google.firebase:firebase-bom:33.5.0"))
+    implementation("com.google.firebase:firebase-analytics")
+
+    implementation("com.google.firebase:firebase-auth")
 }
--- app/src/main/java/com/example/a2024_kotiln_project/SignUpActivity.kt
+++ app/src/main/java/com/example/a2024_kotiln_project/SignUpActivity.kt
@@ -1,20 +1,54 @@
 package com.example.a2024_kotiln_project
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.a2024_kotiln_project.databinding.ActivityLoginBinding
+import com.example.a2024_kotiln_project.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignUpActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySignupBinding
+
+    lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_signup)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 인증 초기화
+        mAuth = Firebase.auth
+
+        binding.signUpBtn.setOnClickListener {
+
+            val email = binding.emailEdit.text.toString().trim()    // trim() 공백 제거
+            val password = binding.passwordEdit.text.toString().trim()
+
+            signUp(email, password)
         }
     }
+
+    /* 회원가입 함수 구현 */
+    private fun signUp(email: String, password: String) {
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {    // 성공 시 실행할 코드
+                    Toast.makeText(this, "회원가입 성공", Toast.LENGTH_SHORT).show()
+                    val intent: Intent = Intent(this@SignUpActivity, MainActivity::class.java)
+                    startActivity(intent)        // 성공시 메인액티비티로 이동
+                } else {        // 실패 시 실행할 코드
+                    Toast.makeText(this, "회원가입 실패", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
 }
